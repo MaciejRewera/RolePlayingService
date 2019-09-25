58b4c823aea2d17e42276ea41d7a1a95be2ab7d4
Maciej Rewera <22579925+MaciejRewera@users.noreply.github.com>
2019-09-25 13:19:40 +0100

    Add indexes to CharacterStatsRepository

--- app/repositories/CharacterStatsRepository.scala
+++ app/repositories/CharacterStatsRepository.scala
@@ -1,11 +1,14 @@
 package repositories
 
 import models.charactersheet.CharacterStats
+import play.api.Logger
 import play.api.libs.json.Json
 import play.api.libs.json.Json.JsValueWrapper
 import reactivemongo.api.Cursor.FailOnError
 import reactivemongo.api.commands.{WriteConcern, WriteResult}
+import reactivemongo.api.indexes.{Index, IndexType}
 import reactivemongo.api.{DefaultDB, MongoConnection, MongoDriver, ReadPreference}
+import reactivemongo.core.errors.GenericDatabaseException
 import reactivemongo.play.json.ImplicitBSONHandlers.JsObjectDocumentWriter
 import reactivemongo.play.json.collection.JSONCollection
 
@@ -13,6 +16,7 @@ import scala.concurrent.{ExecutionContext, Future}
 
 class CharacterStatsRepository(implicit ec: ExecutionContext) {
 
+  private val logger = Logger(this.getClass)
   private val mongoUri = "mongodb://localhost:27018/role-playing-service"
 
   private val driver = new MongoDriver
@@ -23,7 +27,35 @@ class CharacterStatsRepository(implicit ec: ExecutionContext) {
     db <- connection.database(uri.db.get)
   } yield db
 
-  private def collection: Future[JSONCollection] = database.map(_.collection("character-stats"))
+  private lazy val collection: Future[JSONCollection] = database.map(_.collection("character-stats"))
+
+
+  def indexes: Seq[Index] = Seq(Index(Seq("id" -> IndexType.Ascending), name = Some("idIdx"), unique = true))
+
+  def ensureIndexes(implicit ec: ExecutionContext): Future[Seq[Boolean]] =
+    Future.sequence(indexes.map(ensureIndex))
+
+  private val message: String = "Failed to ensure index"
+
+  private def ensureIndex(index: Index)(implicit ec: ExecutionContext): Future[Boolean] =
+    collection.flatMap(_.indexesManager.create(index))
+      .map(wr => {
+        if (!wr.ok) {
+          val msg = wr.writeErrors.mkString(", ")
+          val maybeMsg = if (msg.contains("E11000")) {
+            // this is for backwards compatibility to mongodb 2.6.x
+            throw GenericDatabaseException(msg, wr.code)
+          } else Some(msg)
+          logger.error(s"$message (${index.eventualName}) : '${maybeMsg.map(_.toString)}'")
+        }
+        wr.ok
+      })
+      .recover {
+        case t =>
+          logger.error(s"$message (${index.eventualName})", t)
+          false
+      }
+
 
   def create(stats: CharacterStats): Future[WriteResult] = collection.flatMap(_.insert.one[CharacterStats](stats))
 
@@ -41,6 +73,7 @@ class CharacterStatsRepository(implicit ec: ExecutionContext) {
         .cursor[CharacterStats](readPreference)
         .collect(maxDocs = -1, FailOnError[List[CharacterStats]]())
     )
+
 
   def remove(query: (String, JsValueWrapper)*)(implicit ec: ExecutionContext): Future[WriteResult] =
     collection.flatMap(_.delete(writeConcern = WriteConcern.Default).one(Json.obj(query: _*)))
