c25fa44ac5094bac9bf609aa291340647d5d0e9c
Maciej Rewera <22579925+MaciejRewera@users.noreply.github.com>
2019-10-25 15:23:21 +0100

    Change the way Skills are read from JSON for CharacterStats
    The reference to correct Characteristic for the Skill is not built
    correctly when reading object from JSON.
    Now, building the whole CharacterStats will provide references to
    existing Characteristics instances.

--- app/models/charactersheet/CharacterStats.scala
+++ app/models/charactersheet/CharacterStats.scala
@@ -5,7 +5,7 @@ import java.util.UUID
 import models.charactersheet.characteristics._
 import models.charactersheet.skills._
 import models.charactersheet.talents.Talent
-import play.api.libs.json.{JsValue, Json}
+import play.api.libs.json._
 
 import scala.collection.mutable.{Seq => MSeq}
 
@@ -17,13 +17,37 @@ case class CharacterStats(
 )
 
 object CharacterStats {
-  implicit val format = Json.format[CharacterStats]
 
   def factory: CharacterStatsFactory = new CharacterStatsFactory()
 
-  // TODO: When building this object from DB, the 'wiring' is not set
+  implicit val format: OFormat[CharacterStats] = new OFormat[CharacterStats] {
+    override def writes(o: CharacterStats): JsObject = Json.obj(
+      "id" -> o.id,
+      "characteristics" -> Json.toJsFieldJsValueWrapper(o.characteristics),
+      "skills" -> Json.toJsFieldJsValueWrapper(o.skills),
+      "talents" -> Json.toJsFieldJsValueWrapper(o.talents)
+    )
 
-  def apply(json: JsValue): CharacterStats = ???
+    override def reads(json: JsValue): JsResult[CharacterStats] = for {
+      id <- (json \ "id").validate[String]
+      characteristics <- (json \ "characteristics").validate[MSeq[Characteristic]]
+      skills <- (json \ "skills").validate[MSeq[Skill]]
+      talents <- (json \ "talents").validate[MSeq[Talent]]
+    } yield CharacterStats(
+      id = id,
+      characteristics = characteristics,
+      skills = buildSkills(skills, characteristics),
+      talents = talents
+    )
 
+    private def buildSkills(skills: MSeq[Skill], characteristics: MSeq[Characteristic]): MSeq[Skill] =
+      skills.map { skill =>
+        Skill(
+          definition = skill.definition,
+          specialisation = skill.specialisation,
+          allCharacteristics = characteristics
+        )
+      }
+  }
 
 }
