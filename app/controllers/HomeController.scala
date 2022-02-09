d7440b26c835fbb46378de440d3e8906fbe13e26
Maciej Rewera <22579925+MaciejRewera@users.noreply.github.com>
2022-02-09 12:21:32 +0000

    Implement baseline for ReactJS

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,11 +1,12 @@
 package controllers
 
+import play.api.Configuration
+import play.api.http.HttpErrorHandler
 import play.api.mvc._
 import repositories.CharacterStatsRepository
 import views.html.index
 
 import javax.inject._
-import scala.concurrent.ExecutionContext.Implicits.global
 
 /**
   * This controller creates an `Action` to handle HTTP requests to the
@@ -14,6 +15,9 @@ import scala.concurrent.ExecutionContext.Implicits.global
 @Singleton
 class HomeController @Inject()(
   cc: ControllerComponents,
+  assets: Assets,
+  errorHandler: HttpErrorHandler,
+  config: Configuration,
   characterStatsRepository: CharacterStatsRepository,
   startPage: index
 ) extends AbstractController(cc) {
@@ -24,10 +28,21 @@ class HomeController @Inject()(
     * will be called when the application receives a `GET` request with
     * a path of `/`.
     */
-  def index: Action[AnyContent] = Action.async { implicit request =>
-    characterStatsRepository.findAll().map { sheets =>
-      Ok(startPage(sheets))
-    }
+  def index: Action[AnyContent] = assets.at("index.html")
+
+  /** This makes sense only if all backend endpoints start with this prefix. In production, it is then returned NOT_FOUND.
+    * But has no effect in dev mode and even non-existing endpoints return index.html
+    */
+  def assetOrDefault(resource: String): Action[AnyContent] = if (resource.startsWith(config.get[String]("apiPrefix"))){
+    Action.async(r => errorHandler.onClientError(r, NOT_FOUND, "Not found"))
+  } else {
+    if (resource.contains(".")) assets.at(resource) else index
   }
+
+//    Action.async { implicit request =>
+//    characterStatsRepository.findAll().map { sheets =>
+//      Ok(startPage(sheets))
+//    }
+//  }
 
 }
