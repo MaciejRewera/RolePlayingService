76d9af2a1398f39b580e24d145e114bf91f4b725
Maciej Rewera <22579925+MaciejRewera@users.noreply.github.com>
2022-03-09 22:06:27 +0000

    Bring Bootstrap CSS into React FE using CDN

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,21 +1,13 @@
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        <h1>Welcome to WFRP IV edition</h1>
+      </div>
+      <div className="row">
+        <h2>Here you can create your character</h2>
+      </div>
     </div>
   );
 }
